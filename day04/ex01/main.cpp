00d906185e8caf061450184f98c118063b95efc6
Yassine Hebbat <yhebbat@e2r13p12.1337.ma>
2022-02-09 21:59:56 +0100

    :/

--- day04/ex01/main.cpp
+++ day04/ex01/main.cpp
@@ -2,6 +2,29 @@
 
 int main()
 {
+    int t = 0;
+    Animal *ani[8];
+    std::cout << "-------------Dogs----------------" << std::endl;
+    while (t < 4)
+    {
+        ani[t] = new Dog();
+        t++;
+    }
+    std::cout << "-------------Cats----------------" << std::endl;
+    while (t < 8)
+    {
+        ani[t] = new Cat();
+        t++;
+    }
+    std::cout << "-------------Delete----------------" << std::endl;
+    t = 0;
+    while (t < 8)
+    {
+        delete ani[t];
+        t++;
+    }
+    std::cout << "------------------------------------" << std::endl;
+
     const Animal *j = new Dog();
     const Animal *i = new Cat();
     
--- day04/ex02/main.cpp
+++ day04/ex02/main.cpp
@@ -2,11 +2,33 @@
 
 int main()
 {
+    int t = 0;
+    Animal *ani[8];
+    std::cout << "-------------Dogs----------------" << std::endl;
+    while (t < 4)
+    {
+        ani[t] = new Dog();
+        t++;
+    }
+    std::cout << "-------------Cats----------------" << std::endl;
+    while (t < 8)
+    {
+        ani[t] = new Cat();
+        t++;
+    }
+    std::cout << "-------------Delete----------------" << std::endl;
+    t = 0;
+    while (t < 8)
+    {
+        delete ani[t];
+        t++;
+    }
+    std::cout << "------------------------------------" << std::endl;
     const Animal *j = new Dog();
-    const Animal *c = new Animal();
+    // const Animal *c = new Animal();
     const Animal *i = new Cat();
     
     delete j;//shouldnotcreatealeak
-    delete c;//shouldnotcreatealeak
+//    delete c;//shouldnotcreatealeak
     delete i;
 }
